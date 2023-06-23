df0ed5a1f78984510f6be092e935da43f68e952a
Maxim <334890@gmail.com>
2023-06-23 20:26:26 +0600

    tdd calculator object

--- index.js
+++ index.js
@@ -17,5 +17,37 @@ export function reverseString(input) {
 }
 
 export const calculator = (function () {
-  return {};
+  function validate(a, b) {
+    for (let input of [a, b]) {
+      if (typeof input !== "number") {
+        return false;
+      }
+    }
+    return true;
+  }
+  function add(a, b) {
+    if (!validate(a, b)) {
+      return undefined;
+    }
+    return a + b;
+  }
+  function subtract(a, b) {
+    if (!validate(a, b)) {
+      return undefined;
+    }
+    return a - b;
+  }
+  function divide(a, b) {
+    if (!validate(a, b)) {
+      return undefined;
+    }
+    return a / b;
+  }
+  function multiply(a, b) {
+    if (!validate(a, b)) {
+      return undefined;
+    }
+    return a * b;
+  }
+  return { add, subtract, divide, multiply };
 })();
